f9e81ee34b3b4c408780fa40e243e5bcd1c9cce3
YUSRI ADIB <muhdyusriadib@gmail.com>
2025-08-28 16:55:04 +0800

    Create directory for model storage and print success
    Added directory creation for model storage and a success message.

--- app/services/train_model.py
+++ app/services/train_model.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sklearn.svm import SVC
 import joblib
+import os
 
 df = pd.read_csv("app/data/models/data.csv")
-print(df.head())
-
 X = df.drop('target', axis=1)
 y = df['target']
 
 model = SVC()
 model.fit(X, y)
+
+os.makedirs("app/data/models", exist_ok=True)
 joblib.dump(model, "app/data/models/model_v1.pkl")
+print("Model saved!")
