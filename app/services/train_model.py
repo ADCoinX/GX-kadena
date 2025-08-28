921f691b463a3560236a351a5754c6fe24959789
YUSRI ADIB <muhdyusriadib@gmail.com>
2025-08-28 14:17:39 +0800

    Refactor comments for clarity in train_model.py

--- app/services/train_model.py
+++ app/services/train_model.py
@@ -8,14 +8,14 @@ def train_and_save_model(path="app/data/models/model_v1.pkl"):
     X_train, y_train = make_classification(
         n_samples=500, n_features=5, n_classes=2, random_state=42
     )
-    # Train model dummy
+    # Train model
     model = RandomForestClassifier(n_estimators=50, random_state=42)
     model.fit(X_train, y_train)
 
-    # Pastikan folder wujud
+    # Pastikan folder ada
     os.makedirs(os.path.dirname(path), exist_ok=True)
     joblib.dump(model, path)
-    print(f"✅ Model siap! File di: {path}")
+    print(f"✅ Model siap disimpan di: {path}")
 
 if __name__ == "__main__":
     train_and_save_model()
