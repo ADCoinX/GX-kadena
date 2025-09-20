9b8a24ca0566a596a260019bd0cd2a220d065e03
YUSRI ADIB <muhdyusriadib@gmail.com>
2025-09-20 17:08:01 +0800

    Delete tests directory

--- tests/test_logger.py
+++
@@ -1,6 +0,0 @@
-"""Test persistent logger."""
-from app.services.logger import setup_logging, log_event
-
-def test_log_event():
-    setup_logging()
-    log_event(event="test", details="unit test log")
